b2a17ac9183c0e3266bfe6f80244cde4722ed5e8
saniok333 <alex.o.csc@gmail.com>
2022-07-06 18:23:49 +0300

    Added Ex from Section3

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -2,24 +2,24 @@ import { StyleSheet, FlatList, Text } from 'react-native';
 
 export const ListScreen = () => {
   const friends = [
-    { name: 'Friend #1' },
-    { name: 'Friend #2' },
-    { name: 'Friend #3' },
-    { name: 'Friend #4' },
-    { name: 'Friend #5' },
-    { name: 'Friend #6' },
-    { name: 'Friend #7' },
-    { name: 'Friend #8' },
-    { name: 'Friend #9' },
-    { name: 'Friend #10' },
+    { name: 'Friend #1', age: 20 },
+    { name: 'Friend #2', age: 30 },
+    { name: 'Friend #3', age: 50 },
+    { name: 'Friend #4', age: 34 },
+    { name: 'Friend #5', age: 35 },
+    { name: 'Friend #6', age: 54 },
+    { name: 'Friend #7', age: 13 },
+    { name: 'Friend #8', age: 23 },
+    { name: 'Friend #9', age: 37 },
+    { name: 'Friend #10', age: 55 },
   ];
   return (
     <FlatList
-      horizontal
-      showsHorizontalScrollIndicator={false}
       data={friends}
       renderItem={({ item }) => (
-        <Text style={styles.textStyle}>{item.name}</Text>
+        <Text style={styles.textStyle}>
+          {item.name} - Age {item.age}
+        </Text>
       )}
       keyExtractor={(friend) => friend.name}
     />
