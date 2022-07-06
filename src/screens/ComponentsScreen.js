19dc6f95c1ac128cc695747e3f3dce79cdcca18c
saniok333 <alex.o.csc@gmail.com>
2022-07-06 15:45:41 +0300

    Added custom component

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text } from 'react-native';
+
+export const ComponentsScreen = () => {
+  return <Text style={styles.textStyle}>Components Screen</Text>;
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 50,
+  },
+});
