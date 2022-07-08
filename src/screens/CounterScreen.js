d4afb8fae5fe4260e095297bc03026116efa40da
saniok333 <alex.o.csc@gmail.com>
2022-07-08 12:37:17 +0300

    Added Counter screen

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export const CounterScreen = () => {
+  return (
+    <View>
+      <Text>CounterScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
