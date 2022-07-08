2a37e4b8698c8c30adba7ad78e6e4df430697e46
saniok333 <alex.o.csc@gmail.com>
2022-07-08 13:23:45 +0300

    Counter implementation

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <View>
-      <Text>CounterScreen</Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          setCounter((currentCounter) => currentCounter + 1);
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter((currentCounter) => currentCounter - 1);
+        }}
+      />
+      <Text>Current Count: {counter}</Text>
     </View>
   );
 };
