636cd6a64bb80a2f617e574b1b769fd837bf44ed
saniok333 <alex.o.csc@gmail.com>
2022-07-12 16:29:03 +0300

    Added TextScreen functionality

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import { StyleSheet, View, Text, TextInput } from 'react-native';
 
 export const TextScreen = () => {
+  const [name, setName] = useState('');
   return (
     <View>
+      <Text>Enter your name</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
       />
+      <Text>My name is {name}</Text>
     </View>
   );
 };
