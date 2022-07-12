6c6b34736aefa979d257f0fa021c1e33b5ba147d
saniok333 <alex.o.csc@gmail.com>
2022-07-12 16:40:25 +0300

    Added password lenth chacking for  TextScreen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,18 +2,20 @@ import { useState } from 'react';
 import { StyleSheet, View, Text, TextInput } from 'react-native';
 
 export const TextScreen = () => {
-  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <View>
-      <Text>Enter your name</Text>
+      <Text>Enter password</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value={name}
-        onChangeText={(newValue) => setName(newValue)}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
       />
-      <Text>My name is {name}</Text>
+      {password.length < 5 && (
+        <Text>Password must be longer than 5 characters</Text>
+      )}
     </View>
   );
 };
