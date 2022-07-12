621e1cd010ac33e3f876b0e14418a3f2d521f7b1
saniok333 <alex.o.csc@gmail.com>
2022-07-12 10:55:42 +0300

    SquareScreen (adjusting colors) functionality implementation with a help of useReducer hook

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,28 +1,11 @@
 import { StyleSheet, View, Text, Button } from 'react-native';
 
-export const ColorCounter = ({ color, setter }) => {
-  const COLOR_INCREMENT = 5;
-  const MAX_COLOR_VALUE = 255;
-  const MIN_COLOR_VALUE = 0;
-  const increaseColorValue = () => {
-    setter((currentCounter) =>
-      currentCounter + COLOR_INCREMENT > MAX_COLOR_VALUE
-        ? MAX_COLOR_VALUE
-        : currentCounter + COLOR_INCREMENT
-    );
-  };
-  const decreaseColorValue = () => {
-    setter((currentCounter) =>
-      currentCounter - COLOR_INCREMENT < MIN_COLOR_VALUE
-        ? MIN_COLOR_VALUE
-        : currentCounter - COLOR_INCREMENT
-    );
-  };
+export const ColorCounter = ({ color, onIncrease, onDecrease }) => {
   return (
     <View>
       <Text>{color}</Text>
-      <Button title={`Increase ${color}`} onPress={increaseColorValue} />
-      <Button title={`Decrease ${color}`} onPress={decreaseColorValue} />
+      <Button title={`Increase ${color}`} onPress={onIncrease} />
+      <Button title={`Decrease ${color}`} onPress={onDecrease} />
     </View>
   );
 };
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,25 +1,78 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { ColorCounter } from '../components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
+const MAX_COLOR_VALUE = 255;
+const MIN_COLOR_VALUE = 0;
+
+const reducer = (state, action) => {
+  // state === {red: number, green: number, blue: number}
+  // action === { type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15 }
+
+  const nextValue = state[action.type] + action.payload;
+
+  const calculateValue = () => {
+    if (nextValue < MIN_COLOR_VALUE) return MIN_COLOR_VALUE;
+    if (nextValue > MAX_COLOR_VALUE) return MAX_COLOR_VALUE;
+    return nextValue;
+  };
+
+  switch (action.type) {
+    case 'change_red':
+      return { ...state, red: calculateValue() };
+    case 'change_green':
+      return { ...state, green: calculateValue() };
+    case 'change_blue':
+      return { ...state, blue: calculateValue() };
+    default:
+      return state;
+  }
+};
+
 export const SquareScreen = () => {
-  const [redCounter, setRedCounter] = useState(125);
-  const [greenCounter, setGreenCounter] = useState(125);
-  const [blueCounter, setBlueCounter] = useState(125);
+  const [state, dispatch] = useReducer(reducer, {
+    red: 125,
+    green: 125,
+    blue: 125,
+  });
+  const { red, green, blue } = state;
 
   return (
     <View>
-      <Text>{redCounter}</Text>
-      <ColorCounter color="Red" setter={setRedCounter} />
-      <Text>{greenCounter}</Text>
-      <ColorCounter color="Green" setter={setGreenCounter} />
-      <Text>{blueCounter}</Text>
-      <ColorCounter color="Blue" setter={setBlueCounter} />
+      <Text>{red}</Text>
+      <ColorCounter
+        color="Red"
+        onIncrease={() =>
+          dispatch({ type: 'change_red', payload: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ type: 'change_red', payload: -1 * COLOR_INCREMENT })
+        }
+      />
+      <ColorCounter
+        color="Green"
+        onIncrease={() =>
+          dispatch({ type: 'change_green', payload: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ type: 'change_green', payload: -1 * COLOR_INCREMENT })
+        }
+      />
+      <ColorCounter
+        color="Blue"
+        onIncrease={() =>
+          dispatch({ type: 'change_blue', payload: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ type: 'change_blue', payload: -1 * COLOR_INCREMENT })
+        }
+      />
       <View
         style={{
           height: 100,
           width: 100,
-          backgroundColor: `rgb(${redCounter}, ${greenCounter}, ${blueCounter})`,
+          backgroundColor: `rgb(${red}, ${green}, ${blue})`,
         }}
       />
     </View>
