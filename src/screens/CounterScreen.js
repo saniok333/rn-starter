6de2a5a7f94225e2e7babf031c5ee1d658a9e10f
saniok333 <alex.o.csc@gmail.com>
2022-07-12 11:25:23 +0300

    CounterScreen functionality implementation with a help of useReducer hook

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,20 +1,34 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
+const COUNTER_INCREMENT = 1;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increase':
+      return { ...state, counter: state.counter + action.payload };
+    case 'decrease':
+      return { ...state, counter: state.counter - action.payload };
+    default:
+      return state;
+  }
+};
+
 export const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  const { counter } = state;
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          setCounter((currentCounter) => currentCounter + 1);
+          dispatch({ type: 'increase', payload: COUNTER_INCREMENT });
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          setCounter((currentCounter) => currentCounter - 1);
+          dispatch({ type: 'decrease', payload: COUNTER_INCREMENT });
         }}
       />
       <Text>Current Count: {counter}</Text>
