e161976f77a99936eb80ea4b08bcbb61c96954e1
saniok333 <alex.o.csc@gmail.com>
2022-07-12 18:19:42 +0300

    Flexbox model implementation

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,7 +3,9 @@ import { StyleSheet, View, Text, TextInput } from 'react-native';
 export const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>BoxScreen</Text>
+      <Text style={[styles.textStyle, styles.textStyle1]}>Child #1</Text>
+      <Text style={[styles.textStyle, styles.textStyle2]}>Child #2</Text>
+      <Text style={[styles.textStyle, styles.textStyle3]}>Child #3</Text>
     </View>
   );
 };
@@ -12,10 +14,24 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 1,
     borderColor: 'black',
+    height: 200,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   textStyle: {
-    borderWidth: 10,
+    borderWidth: 3,
     borderColor: 'red',
-    margin: 20,
+  },
+  textStyle1: {
+    flex: 1,
+    alignSelf: 'flex-start',
+  },
+  textStyle2: {
+    flex: 2,
+  },
+  textStyle3: {
+    flex: 1,
+    alignSelf: 'flex-end',
   },
 });
